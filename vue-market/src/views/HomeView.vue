b48a1dd7d76e232158a024ecdd7be9c277bd2aca
nasimiyunamasaka@gmail.com <nasimiyunamasaka@gmail.com>
2026-02-02 23:21:46 +0300

    setting home page

--- vue-market/src/views/HomeView.vue
+++ vue-market/src/views/HomeView.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="home">
-    home
+    <section class="hero is-mediun mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to the cloth market
+        </p>
+        <p class="subtitle">
+          An affordable cloth store
+        </p>
+      </div>
+    </section>
+
+    <div class="column is-multiline">
+      <div class="colum is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
+      </div>
+    </div>
   </div>
 </template>
 
