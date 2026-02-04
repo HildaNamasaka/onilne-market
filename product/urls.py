1a993e25952383a76686c1e5980e44e4962b6c89
nasimiyunamasaka@gmail.com <nasimiyunamasaka@gmail.com>
2026-02-04 10:03:20 +0300

    detail view

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductList
+from .views import ProductList, ProductDetail
 
 urlpatterns = [
-    path('products/', ProductList.as_view()),
+    path('products/', ProductList.as_view(), name='product-list'),
+    path('details/<slug:category_slug>/<slug:product_slug>/', ProductDetail.as_view(), name='product-details')
 ]
